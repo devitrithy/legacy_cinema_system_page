b216c8918de2ab0c5d675d924e17acf0e0aaf43e
devit2000 <ditway1405@gmail.com>
2023-07-17 18:18:07 +0700

    movie detail

--- src/routes/movie/[id]/+page.svelte
+++ src/routes/movie/[id]/+page.svelte
@@ -19,7 +19,7 @@
   let endpoint = "https://cinemaapi.serveo.net/";
 </script>
 
-<main class="m-5">
+<main class="m-5 container text-black dark:text-white">
   <h1 class="text-black dark:text-white text-2xl m-4">
     {$page.url.pathname === "/movie" ? "Movie" : "Add Movie"}
   </h1>
@@ -32,47 +32,25 @@
       >
     </Breadcrumb>
   </div>
-  <div class="flex gap-5 text-black dark:text-white">
+  <div class="flex gap-10 mx-10 mt-5">
     <img
       src={endpoint + movie.poster}
-      style="height: 350px; border-radius: 25px;"
+      class="h-[80vh] rounded-lg shadow-lg shadow-red-900"
       alt={movie.title}
     />
-    <div class="flex flex-col items-start gap-5 justify-start">
-      <p class="flex justify-center items-center gap-5">
-        <VideoCameraOutline />
-        <span>
-          Title: {movie.title}
-        </span>
-      </p>
-      <p class="flex justify-center gap-5">
-        <DesktopPcOutline />
-        <span>
-          Description: {movie.description}
-        </span>
-      </p>
-      <p class="flex justify-center items-center gap-5">
-        <ClockOutline />
-        <span>
-          Time: {movie.time}
-        </span>
-      </p>
-      <p class="flex justify-center items-center gap-5">
-        <TagOutline />
-        <span>
-          Genre: {movie.genre}
-        </span>
-      </p>
+    <div class="flex gap-10 flex-col">
+      <p>{movie.title}</p>
+      <p class="w-[50vw]">{movie.description}</p>
+      <p>{movie.time}</p>
+      <p>{movie.genre}</p>
+      <iframe
+        class="h-full"
+        src="https://www.youtube.com/embed/{movie.trailer}?autoplay=1&mute=1"
+        title={movie.title}
+        frameborder="0"
+        allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      />
     </div>
-  </div>
-  <div>
-    <iframe
-      width="1500"
-      height="600"
-      src="https://www.youtube.com/embed/{movie.trailer}"
-      title={movie.title}
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    />
   </div>
 </main>
