25f3bd96d056902e8c87b6d2c35feb53bf3c3126
devit2000 <ditway1405@gmail.com>
2023-07-18 00:43:58 +0700

    done with loading

--- src/routes/movie/+page.svelte
+++ src/routes/movie/+page.svelte
@@ -67,6 +67,11 @@
       trailer: "",
       time: "",
     };
+    iTime = 0;
+    iTitle = 0;
+    iDescription = 0;
+    iTrailer = 0;
+    iGenre = 0;
   };
 
   import type { SubmitFunction } from "./$types.js";
@@ -131,6 +136,12 @@
 
   //Edit Form
   let editForm = async (id: string) => {
+    iTime = 0;
+    iTitle = 0;
+    iDescription = 0;
+    iTrailer = 0;
+    iTime = 0;
+    iGenre = 0;
     ids = id;
     edit = true;
     const editData = await axios.get(endpoint + "movie/" + id);
@@ -219,13 +230,15 @@
     };
   };
   let deleteMovie: SubmitFunction = ({ form, data, action, cancel }) => {
+    loading = true;
     return async ({ result, update }) => {
+      loading = false;
       switch (result.type) {
         case "success":
           toast.success("Successfully remove the movie.", {
             style: "border-radius: 200px; background: #333; color: #fff;",
           });
-          formModal = false;
+          popupModal = false;
           break;
         default:
           break;
@@ -324,49 +337,66 @@
     <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
       {edit ? "Edit" : "Add"} Movie
     </h3>
-    <Label class="space-y-2">
-      <span>Title</span>
-      {#if iTitle == 1}
-        <small class="text-red-500 float-right"> *Required</small>
-      {/if}
-      <Input
-        color={iTitle == 0 ? "base" : iTitle == 1 ? "red" : "green"}
-        type="text"
-        name="title"
-        bind:value={formInput.title}
-        placeholder="John Wick 4"
-      />
-    </Label>
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+      <Label class="space-y-2">
+        <span>Title</span>
+        <Input
+          color={iTitle == 0 ? "base" : iTitle == 1 ? "red" : "green"}
+          type="text"
+          name="title"
+          bind:value={formInput.title}
+          placeholder="John Wick 4"
+        />
+        {#if iTitle == 1}
+          <Helper class="mt-2" color="red"
+            ><span class="font-medium">Invalid!</span> Title is required!</Helper
+          >
+        {:else if iTitle === 2}
+          <Helper class="mt-2" color="green"
+            ><span class="font-medium">Well done!</span> Title is valid.</Helper
+          >
+        {/if}
+      </Label>
+      <Label class="space-y-2">
+        <span>Genre</span>
+        <Input
+          color={iGenre == 0 ? "base" : iGenre == 1 ? "red" : "green"}
+          type="text"
+          name="genre"
+          bind:value={formInput.genre}
+          placeholder="Action"
+        />
+        {#if iGenre == 1}
+          <Helper class="mt-2" color="red"
+            ><span class="font-medium">Invalid!</span> Genre is required!</Helper
+          >
+        {:else if iGenre === 2}
+          <Helper class="mt-2" color="green"
+            ><span class="font-medium">Well done!</span> Genre is valid.</Helper
+          >
+        {/if}
+      </Label>
+    </div>
     <Label class="space-y-2">
       <span>Description</span>
-      {#if iDescription == 1}
-        <small class="text-red-500 float-right"> *Required</small>
-      {/if}
       <Textarea
         type="text"
         name="description"
         bind:value={formInput.description}
         placeholder="John Wick 4 is an action movie..."
       />
-    </Label>
-    <Label class="space-y-2">
-      <span>Genre</span>
-      {#if iGenre == 1}
-        <small class="text-red-500 float-right">*Required</small>
+      {#if iDescription == 1}
+        <Helper class="mt-2" color="red"
+          ><span class="font-medium">Invalid!</span> Description is required!</Helper
+        >
+      {:else if iDescription === 2}
+        <Helper class="mt-2" color="green"
+          ><span class="font-medium">Well done!</span> Description is valid.</Helper
+        >
       {/if}
-      <Input
-        color={iGenre == 0 ? "base" : iGenre == 1 ? "red" : "green"}
-        type="text"
-        name="genre"
-        bind:value={formInput.genre}
-        placeholder="Action"
-      />
     </Label>
     <Label class="space-y-2">
       <span>Time</span>
-      {#if iTime == 1}
-        <small class="text-red-500 float-right">*Required</small>
-      {/if}
       <Input
         color={iTime == 0 ? "base" : iTime == 1 ? "red" : "green"}
         type="text"
@@ -374,12 +404,18 @@
         bind:value={formInput.time}
         placeholder="2h40min"
       />
+      {#if iTime == 1}
+        <Helper class="mt-2" color="red"
+          ><span class="font-medium">Invalid!</span> Time is required!</Helper
+        >
+      {:else if iTime === 2}
+        <Helper class="mt-2" color="green"
+          ><span class="font-medium">Well done!</span> Time is valid.</Helper
+        >
+      {/if}
     </Label>
     <Label class="space-y-2">
       <span>Trailer Link</span>
-      {#if iTrailer == 1}
-        <small class="text-red-500 float-right">*Required</small>
-      {/if}
       <Input
         color={iTrailer == 0 ? "base" : iTrailer == 1 ? "red" : "green"}
         type="text"
@@ -387,6 +423,15 @@
         bind:value={formInput.trailer}
         placeholder="Trailer video link..."
       />
+      {#if iTrailer == 1}
+        <Helper class="mt-2" color="red"
+          ><span class="font-medium">Invalid!</span> Trailer is required!</Helper
+        >
+      {:else if iTrailer === 2}
+        <Helper class="mt-2" color="green"
+          ><span class="font-medium">Well done!</span> Trailer is valid.</Helper
+        >
+      {/if}
     </Label>
     <Label for="with_helper" class="pb-2">Upload file</Label>
     <Fileupload
@@ -406,7 +451,7 @@
       <Button type="button" color="red" on:click={resetValue} class="w-full"
         >Reset</Button
       >
-      <Button type="submit" color="green" class="w-full">
+      <Button type="submit" color="green" class="w-full" shadow>
         {#if loading}<Spinner class="mr-3" size="4" color="white" /> Submitting{:else}Submit{/if}
       </Button>
     </div>
@@ -414,32 +459,31 @@
 </Modal>
 <Modal bind:open={popupModal} size="xs" autoclose={false}>
   <div class="text-center">
-    <svg
-      aria-hidden="true"
-      class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      /></svg
-    >
+    {#if loading}
+      <div class="my-5">
+        <Spinner color="gray" size={8} />
+      </div>
+    {:else}
+      <svg
+        aria-hidden="true"
+        class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        /></svg
+      >
+    {/if}
     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
       Are you sure you want to delete this movie?
     </h3>
     <form action="?/delete" method="post" use:enhance={deleteMovie}>
-      <Button
-        type="submit"
-        on:click={() => {
-          popupModal = false;
-        }}
-        color="red"
-        class="mr-2">Yes, I'm sure</Button
-      >
+      <Button type="submit" color="red" class="mr-2">Yes, I'm sure</Button>
       <Button
         type="button"
         on:click={() => {
