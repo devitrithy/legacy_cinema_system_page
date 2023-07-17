dd43fe524ace31b9b3addda98cb9653f379c2cff
devit2000 <ditway1405@gmail.com>
2023-07-17 14:26:15 +0700

    update on error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div
+  class="flex flex-col gap-4 items-center justify-center text-black dark:text-white"
+>
+  {#if $page.status === 404}
+    <h1>{$page.status} {$page.error.message}</h1>
+    <span style="font-size: 10em">
+      {$page.status}
+    </span>
+    <p>
+      You’re either misspelling the URL or requesting a page that's no longer
+      here.
+    </p>
+    <a href="/" class="underline">Click here to go to home page</a>
+  {:else}
+    <h1>{$page.status} {$page.error.message}</h1>
+    <span style="font-size: 10em">
+      {$page.status}
+    </span>
+    <p>Something went wrong with the connection please try again later.</p>
+    <a href="/" class="underline">Click here to go to home page</a>
+  {/if}
+</div>
