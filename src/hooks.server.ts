67d6498dd7206510894b9a8d80152995d4c9a7c1
devit2000 <ditway1405@gmail.com>
2023-08-03 23:34:37 +0700

    error minh

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,20 +1,9 @@
+import { auth } from "$lib/auth";
 import { redirect, type Handle } from "@sveltejs/kit";
-import { verify } from "jsonwebtoken";
 
 export const handle: Handle = async ({ event, resolve }) => {
   //1
-  const token = event.cookies.get("token");
-  if (token) {
-    let user = verify(token, "anh thunh jm aii");
-    event.locals.user = {
-      user_id: user.user_id,
-      profile: user.profile,
-      firstname: user.firstname,
-      lastname: user.lastname,
-      email: user.email,
-      username: user.username,
-    };
-  }
+  event.locals.user = auth(event);
   if (
     event.url.pathname.startsWith("/") &&
     !event.url.pathname.startsWith("/login")
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,22 +1,17 @@
-// src/auth.js
+import { verify } from "jsonwebtoken";
 
-import { writable } from "svelte/store";
-
-// Create a writable store to manage authentication state
-export const isAuthenticated = writable(false);
-export const currentUser = writable(null);
-
-// Function to log in a user
-export async function login(username: String, password: String) {
-  // Call your authentication service or API to validate the credentials
-  // If authentication is successful, set isAuthenticated to true and store user data
-  isAuthenticated.set(true);
-  currentUser.set({ username: username }); // You can store more user data as needed
-}
-
-// Function to log out a user
-export async function logout() {
-  // Perform any cleanup or logout actions
-  isAuthenticated.set(false);
-  currentUser.set(null);
-}
+export const auth = (event) => {
+  const token = event.cookies.get("token");
+  if (token) {
+    let user = verify(token, "anh thunh jm aii");
+    return {
+      user_id: user.user_id,
+      profile: user.profile,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      username: user.username,
+    };
+  }
+  return null;
+};
