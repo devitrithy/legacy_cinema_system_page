caa03d7dd3ca3362850f8608ab680a7e59b25bec
devit2000 <ditway1405@gmail.com>
2023-08-10 04:53:43 +0700

    deploy

--- src/routes/(app)/ticket/+page.server.ts
+++ src/routes/(app)/ticket/+page.server.ts
@@ -1,8 +1,14 @@
+import { NODE_ENV } from "$env/static/private";
 import { PUBLIC_API_ENDPOINT } from "$env/static/public";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ cookies, url }) => {
-  let date = url.searchParams.get("day") || new Date().getDate() + 1;
+  let date;
+  if (NODE_ENV === "development") {
+    date = url.searchParams.get("day") || new Date().getDate();
+  } else {
+    date = url.searchParams.get("day") || new Date().getDate() + 1;
+  }
   const token = cookies.get("token");
   const customHeaders = {
     Authorization: "Bearer " + token, // Replace 'YOUR_ACCESS_TOKEN' with your actual access token
