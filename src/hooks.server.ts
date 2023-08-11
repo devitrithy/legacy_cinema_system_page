0e4c1923892091b9172edb881105e86cc83752b5
devit2000 <ditway1405@gmail.com>
2023-08-11 14:35:10 +0700

    bart

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,18 +4,11 @@ import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
   //1
-  const token = event.cookies.get("token");
-  const customHeaders = {
-    Authorization: "Bearer " + token, // Replace 'YOUR_ACCESS_TOKEN' with your actual access token
-  };
-  const authData = await fetch(`${PUBLIC_API_ENDPOINT}/user/callback`, {
-    headers: customHeaders,
-  });
   event.locals.user = auth(event);
   const response = await resolve(event); //2
   //3
   if (!event.url.pathname.startsWith("/login")) {
-    if (authData.status !== 200) {
+    if (!event.locals.user) {
       const fromUrl = event.url.pathname + event.url.search;
       throw redirect(303, `/login?r=${fromUrl}`);
     }
